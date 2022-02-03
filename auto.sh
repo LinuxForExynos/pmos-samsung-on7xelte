498d74a29da59fa2f18efb303cc360ea6c4cd0f6
Otus9051 <58171861+Otus9051@users.noreply.github.com>
2022-02-03 14:55:34 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -17,7 +17,7 @@ echo "                 _                        _        _    ___  ____
  / ___ \ |_| | || (_) \ V  V / (_) | |  |   <  __/ |   
 /_/   \_\__,_|\__\___/ \_/\_/ \___/|_|  |_|\_\___|_|  "
 
-pmos='/tmp/cirrus-ci-build/pmos'
+pmos='/tmp/cirrus-ci-build'
 
 echo " "
 echo "You will be asked for SUDO/DOAS password"
