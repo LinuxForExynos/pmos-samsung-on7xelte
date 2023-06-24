89158fdc408e178adc970016dedee3855ca35921
Otus9051 <stevewholikesthings@gmail.com>
2023-06-24 14:16:15 +0530

    Update Device Tree, Fix auto.sh

--- auto.sh
+++ auto.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-echo " _     _                  _____         _____ ___ _____ ___  
-| |   (_)_ __  _   ___  _|  ___|__  _ _|___  ( _ )___  / _ \ 
-| |   | | '_ \| | | \ \/ / |_ / _ \| '__| / // _ \  / / | | |
-| |___| | | | | |_| |>  <|  _| (_) | |   / /| (_) |/ /| |_| |
-|_____|_|_| |_|\__,_/_/\_\_|  \___/|_|  /_/  \___//_/  \___/ 
+echo "  _     _                  _____          _____                            
+| |   (_)_ __  _   ___  _|  ___|__  _ __| ____|_  ___   _ _ __   ___  ___ 
+| |   | | '_ \| | | \ \/ / |_ / _ \| '__|  _| \ \/ / | | | '_ \ / _ \/ __|
+| |___| | | | | |_| |>  <|  _| (_) | |  | |___ >  <| |_| | | | | (_) \__ \
+|_____|_|_| |_|\__,_/_/\_\_|  \___/|_|  |_____/_/\_\\__, |_| |_|\___/|___/
+                                                    |___/                 
 "
 echo "                 _                        _        _    ___  ____  
  _ __   ___  ___| |_ _ __ ___   __ _ _ __| | _____| |_ / _ \/ ___| 
@@ -21,25 +22,21 @@ if [ "$CI" = "true" ]; then
     echo "\nCI Process detected";
     CI_PMBTSP="/root/.local/var/pmbootstrap"
     CI_PM_EXTRA_ARGS="--as-root";
-else 
-    CI_PMBTSP="$HOME/.local/var/pmbootstrap";
 fi
+
+echo "Please enter the directory pmbootstrap has been initialized in: "
+read -r CI_PMBTSP
 echo "Using installation $CI_PMBTSP"
-
-
 echo " "
-echo "You will be asked for SUDO/DOAS password"
-sudo pacman -Syu --noconfirm
+echo "You may be asked for your password several times during the build process"
 echo " "
 echo "Copying Source Trees"
-echo " "
 rm -rf $CI_PMBTSP/cache_git/pmaports/device/testing/linux-samsung-on7xelte
 rm -rf $CI_PMBTSP/cache_git/pmaports/device/testing/device-samsung-on7xelte
 cp -r linux-samsung-on7xelte $CI_PMBTSP/cache_git/pmaports/device/testing/
 cp -r device-samsung-on7xelte $CI_PMBTSP/cache_git/pmaports/device/testing/
 cp -r firmware-samsung-on7xelte $CI_PMBTSP/cache_git/pmaports/device/testing/
 
-echo " "
 echo "Doing Checksums"
 echo " "
 pmbootstrap $CI_PM_EXTRA_ARGS checksum linux-samsung-on7xelte 
@@ -64,8 +61,7 @@ echo "Copying to $PWD"
 echo " "
 
 cp -rv $CI_PMBTSP/chroot_rootfs_samsung-on7xelte/boot/* .
-cp -rv $CI_PMBTSP/chroot_native/home/pmos/rootfs/samsung-on7xelte.img .
-cp -rv $CI_PMBTSP/chroot_buildroot_aarch64/var/lib/postmarketos-android-recovery-installer/pmos-samsung-on7xelte.zip 
+cp -rv $CI_PMBTSP/chroot_buildroot_aarch64/var/lib/postmarketos-android-recovery-installer/pmos-samsung-on7xelte.zip .
 			
 echo " "
 echo "Done! Flash with the instructions given on https://forum.xda-developers.com/t/dev-linux-alpha-build-released-postmarketos-for-g610f.4392165/"
